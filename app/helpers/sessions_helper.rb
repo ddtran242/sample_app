f898fd46f5beb0e732604a4cc0f5a4a81c202352
Duy Tran <duuyyy242@gmail.com>
2017-03-09 15:05:20 +0700

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,10 +3,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    @current_user ||= User.find_by id: session[:user_id]
-  end
-
   def logged_in?
     current_user.present?
   end
@@ -28,7 +24,7 @@ module SessionsHelper
       @current_user ||= User.find_by id: user_id
     elsif user_id = cookies.signed[:user_id]
       user = User.find_by id: user_id
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,20 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method :smtp
+  host = "localhost"
+  config.action_mailer.default_url_options = {host: host, port: 3000}
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: "587",
+    authentication: :plain,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    domain: "gmail.com",
+    enable_starttls_auto: true
+  }
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.perform_caching = false
 
